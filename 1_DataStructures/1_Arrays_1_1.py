749cd0764eabcbc8d01b6124ec28027a963bbf11
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-27 12:16:26 -0800

    Update 1_Arrays_1_1.py

--- 1_DataStructures/1_Arrays_1_1.py
+++ 1_DataStructures/1_Arrays_1_1.py
@@ -27,3 +27,47 @@ def result(a):
     
 print(result(a))
 '''
+# Solution
+b=[1,2,3,4,5]
+
+def products(nums):
+    prefix_products=[]
+    
+    for num in nums:
+        if prefix_products:
+            prefix_products.append(prefix_products[-1]*num)
+            
+        else:
+            prefix_products.append(num)
+            
+    #print(prefix_products)
+    
+    suffix_products=[]
+    
+    for num in reversed(nums):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1]*num)
+            
+        else:
+            suffix_products.append(num)
+            
+    suffix_products=list(reversed(suffix_products))
+    #print(suffix_products)
+    
+    result=[]
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suffix_products[i+1])
+        elif i==len(nums)-1:
+            result.append(prefix_products[i-1])
+        else:
+            result.append(prefix_products[i-1]*suffix_products[i+1])
+            
+    #print(result)
+    return result
+        
+
+        
+
+    
+print(products(b))
