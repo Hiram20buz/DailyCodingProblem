f7a0e896854556f715f82f50e5ad55b6b46cc411
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-28 11:46:29 -0800

    Update 1_Arrays_1_2.py

--- 1_DataStructures/1_Arrays_1_2.py
+++ 1_DataStructures/1_Arrays_1_2.py
@@ -8,6 +8,22 @@ For example,given [3,7,5,6,9],you should return (1,3).
 '''
 #Solution 1
 '''
+def window(array):
+    left,right=None,None
+    s=sorted(array)
+    
+    for i in range(len(array)):
+        if array[i] != s[i] and left is None:
+            left=1
+            #print(left)
+            
+        elif array[i] != s[i]:
+            right=i
+            #print(right)
+            
+    return left,right
+    
+print(window([3,7,5,6,9]))
 
 
 
