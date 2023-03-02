470ed6a27c887d05501578052e53b66ebce3c3db
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-03-02 08:13:22 -0800

    Create 1_Arrays_1_4.py

---
+++ 1_DataStructures/1_Arrays_1_4.py
@@ -0,0 +1,7 @@
+# 1.4 Find number of smaller elements to the rigth
+'''
+Given an array of integers,return a new array where each element in the new array
+is the number of smaller elements to the rigth of that element in the original input array.
+
+For example,given the array [3,4,9,6,1],return [1,1,2,1,0]
+'''
