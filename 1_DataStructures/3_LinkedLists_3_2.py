4a9b74a3c8d74afbb9dcacbb17e43fb5d6624dc9
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-03-02 23:14:55 -0800

    Create 3_LinkedLists_3_2.py

---
+++ 1_DataStructures/3_LinkedLists_3_2.py
@@ -0,0 +1,18 @@
+# 3.2 Add two linked lists that represent numbers
+'''
+We can represent an integer in a linked list format by having each node represent a digit in the number.
+The nodes are connected in reverse order,such that the number 54321 is represented by the following
+linked list:
+
+1 -> 2 -> 3 -> 4 -> 5 
+Given two linked lists in this format,return their sum.
+For example,given:
+
+9->9
+5->2
+
+You should return 124=99+25 as :
+4-> 2-> 1
+
+'''
+
